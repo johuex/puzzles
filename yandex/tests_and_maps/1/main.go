c9ff475d85938a531bf959ca57cfb1c5cc32882e
johuex <polds@list.ru>
2025-09-29 22:34:47 +0300

    added yandex task, tests_and_maps, 1

---
+++ yandex/tests_and_maps/1/main.go
@@ -0,0 +1,63 @@
+package mushrooms
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	// general
+	reader := bufio.NewReaderSize(os.Stdin, 1<<20)
+	writer := bufio.NewWriterSize(os.Stdout, 1<<20)
+	defer writer.Flush()
+
+	// input
+	line, _ := reader.ReadString('\n')
+	n, _ := strconv.Atoi(strings.TrimSpace(line))
+	line, _ = reader.ReadString('\n')
+	line = strings.TrimSpace(line)
+	lineArr := strings.Split(line, " ")
+	weight := make([]int, n)
+	for i := 0; i < len(lineArr); i++ {
+		conv, _ := strconv.Atoi(lineArr[i])
+		weight[i] = conv
+	}
+
+	// logic
+	// нужно найти максимум четных и минимум нечетных и свапнуть их
+	// если же максимум <= минимуму, то ничего менять не нужно
+	// параллельно можно считать сумму для нечетных и сумму четных а затем на основе условия swap пересчитать суммы и вывести индекс радости
+	ans := mushroomsJoy(n, weight)
+	// output
+	writer.WriteString(strconv.Itoa(ans))
+	writer.WriteByte('\n')
+}
+
+func mushroomsJoy(n int, weight []int) int {
+	oddSum := 0  // Вася (0,2,..)
+	evenSum := 0 // Маша (1,3,...)
+	// n <= 2, ок
+	oddMin := weight[0]
+	evenMax := weight[1]
+	for i := range n {
+		if i%2 == 0 {
+			// считаем Васю
+			oddSum += weight[i]
+			oddMin = min(oddMin, weight[i])
+		} else {
+			// считаем Машу
+			evenSum += weight[i]
+			evenMax = max(evenMax, weight[i])
+		}
+	}
+
+	var ans int
+	if oddMin <= evenMax {
+		ans = (oddSum - oddMin + evenMax) - (evenSum - evenMax + oddMin)
+	} else {
+		ans = oddSum - evenSum
+	}
+	return ans
+}
---
+++ yandex/tests_and_maps/1/main_test.go
@@ -0,0 +1,22 @@
+package mushrooms
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var testCases = []map[string]any{
+	{"name": "1", "input": []any{2, []int{1, 2}}, "output": 1},
+	{"name": "2", "input": []any{3, []int{2, 2, 2}}, "output": 2},
+	{"name": "3", "input": []any{11, []int{4, 10, 7, 5, 4, 5, 3, 8, 3, 2, 5}}, "output": 10},
+}
+
+func TestMushroomJoy(t *testing.T) {
+	for _, testCase := range testCases {
+		t.Run(testCase["name"].(string), func(t *testing.T) {
+			res := mushroomsJoy(testCase["input"].([]any)[0].(int), testCase["input"].([]any)[1].([]int))
+			assert.Equal(t, testCase["output"], res)
+		})
+	}
+}
