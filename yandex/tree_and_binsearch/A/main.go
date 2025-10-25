d2276dd2c798e379262b6ed5e3d8ecb256f2ccf8
johuex <polds@list.ru>
2025-10-25 15:54:33 +0300

    tree A yandex, not full

---
+++ yandex/tree_and_binsearch/A/main.go
@@ -0,0 +1,59 @@
+package a
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	// general
+	reader := bufio.NewReaderSize(os.Stdin, 1<<20)
+	writer := bufio.NewWriterSize(os.Stdout, 1<<20)
+	defer writer.Flush()
+
+	// input
+	line, _ := reader.ReadString('\n')
+	line = strings.TrimSpace(line)
+	lineArr := strings.Split(line, " ")
+	a, _ := strconv.Atoi(lineArr[0])
+	b, _ := strconv.Atoi(lineArr[1])
+	s, _ := strconv.Atoi(lineArr[2])
+
+	// logic
+	ans := shelf(a, b, s)
+
+	// output
+	writer.WriteString(strconv.Itoa(ans))
+	writer.WriteByte('\n')
+}
+
+func binSearch(l, r int, check func(int, []int) bool, params []int) int {
+	for l < r {
+		m := (l + r) / 2
+		if check(m, params) {
+			r = m
+		} else {
+			l = m + 1
+		}
+	}
+	return l
+}
+
+func square(l int, params []int) bool {
+	a, b, s := params[0], params[1], params[2]
+	return l*l-l*(a+b)+a*b >= s
+}
+
+func shelf(a, b, s int) int {
+	rightBorder := max(a, b) * 10
+	minBorder := 0
+	params := []int{a, b, s}
+	res := binSearch(minBorder, rightBorder, square, params)
+	if res*res-res*(a+b)+a*b != s {
+		return -1
+	}
+
+	return res
+}
---
+++ yandex/tree_and_binsearch/A/main_test.go
@@ -0,0 +1,31 @@
+package a
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var testCases = []map[string]any{
+	{"name": "1", "input": []any{1, 1, 1}, "output": 2},
+	{"name": "2", "input": []any{2, 3, 12}, "output": 6},
+	{"name": "3", "input": []any{1, 1, 2}, "output": -1},
+	{"name": "4", "input": []any{0, 2, 3}, "output": 3},
+	{"name": "5", "input": []any{2, 0, 3}, "output": 3},
+	{"name": "6", "input": []any{0, 0, 3}, "output": -1},
+	{"name": "7", "input": []any{0, 0, 1}, "output": -1},
+	{"name": "8", "input": []any{566, 239, 664660}, "output": 1234},
+}
+
+func TestShelf(t *testing.T) {
+	for _, testCase := range testCases {
+		t.Run(testCase["name"].(string), func(t *testing.T) {
+			res := shelf(
+				testCase["input"].([]any)[0].(int),
+				testCase["input"].([]any)[1].(int),
+				testCase["input"].([]any)[2].(int),
+			)
+			assert.Equal(t, testCase["output"], res)
+		})
+	}
+}
