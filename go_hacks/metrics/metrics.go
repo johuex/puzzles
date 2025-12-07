684450af4247b3e68972bdb6563fa06434786a74
johuex <polds@list.ru>
2025-12-07 18:23:24 +0300

    metrics code optimize

--- go_hacks/metrics/metrics.go
+++ go_hacks/metrics/metrics.go
@@ -10,44 +10,29 @@ type I interface {
 	Get() int64
 }
 
-// shard
-type shard struct {
-	value int64
-	_     [40]byte // padding
-}
-
-func newShard() *shard {
-	return &shard{}
-}
-
 type Metrics struct {
-	shards      []*shard
+	shards      []atomic.Int64
 	keyShardMap map[string]int
 }
 
 func NewMetrics(keys []string) *Metrics {
-	nKeys := len(keys)
 	keyShardMap := make(map[string]int)
 	for idx, val := range keys {
 		keyShardMap[val] = idx
 	}
 
-	shards := make([]*shard, nKeys)
-	for i := 0; i < nKeys; i++ {
-		shards[i] = newShard()
-	}
 	return &Metrics{
-		shards:      shards,
+		shards:      make([]atomic.Int64, len(keys)),
 		keyShardMap: keyShardMap,
 	}
 }
 
 func (m *Metrics) Add(key string, v int64) {
 	shardID := m.keyShardMap[key]
-	atomic.AddInt64(&m.shards[shardID].value, v)
+	m.shards[shardID].Add(v)
 }
 
 func (m *Metrics) Get(key string) int64 {
 	shardID := m.keyShardMap[key]
-	return atomic.LoadInt64(&m.shards[shardID].value)
+	return m.shards[shardID].Load()
 }
