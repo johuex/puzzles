77c0b5653e1c8386135183d56927b23154c7e1f4
johuex <polds@list.ru>
2026-01-13 13:57:04 +0300

    added 1084 sql leetcode task

---
+++ leetcode/sql/sql_free/1084/solution.sql
@@ -0,0 +1,5 @@
+select distinct p.product_id, p.product_name -- distinct used to filter multiple sellers
+from Product p
+join Sales s on p.product_id = s.product_id
+where sale_date between '2019-01-01' and '2019-03-31'
+and p.product_id not in (select distinct product_id from Sales where sale_date > '2019-03-31' or sale_date < '2019-01-01')
