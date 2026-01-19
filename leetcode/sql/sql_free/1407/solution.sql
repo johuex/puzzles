9945a430fe1baafd1750acb662748328da7715dc
johuex <polds@list.ru>
2026-01-19 09:17:20 +0300

    added 1407 sql leetcode task

---
+++ leetcode/sql/sql_free/1407/solution.sql
@@ -0,0 +1,8 @@
+-- my solution
+select
+    any_value(u.name) "name",
+    coalesce(sum(r.distance), 0) "travelled_distance"
+from Users u
+left join Rides r on u.id = r.user_id
+group by u.id
+order by 2 desc, 1 asc;
