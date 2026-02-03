ebc6b8d5056a576f252a90fd6a80bfafe0164053
johuex <polds@list.ru>
2026-02-03 14:01:27 +0300

    added 608 sql medium task

---
+++ leetcode/sql/sql_free/608/solution.sql
@@ -0,0 +1,9 @@
+select
+    distinct t1.id,
+    case
+        when t1.p_id is null then 'Root'
+        when t2.id is null then 'Leaf'
+        else 'Inner'
+    end "type"
+from Tree t1
+left join Tree t2 on t1.id = t2.p_id;
